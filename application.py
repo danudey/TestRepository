18dd5d76d8fdd216d3e00fc1fe0149eda827416f
Daniel Fox <danudey@me.com>
2019-07-11 12:54:36 -0700

    Initial commit

---
+++ application.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from jinja2 import Template
+from socket import gethostname
+
+# some bits of text for the page.
+template_data = open('index.html', 'rt').read()
+
+template = Template(template_data)
+output = template.render(hostname=gethostname())
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+
+@application.route('/')
+def index():
+    return output
+
+
+@application.route('/health_check')
+def health_check():
+    return 'Everything is A-OK!'
+
+
+# run the app.
+if __name__ == '__main__':
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
